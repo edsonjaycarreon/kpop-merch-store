26118006008896486a577b63e9b6e2cfcacf4e61
edsonjaycarreon <ejay.carreon@gmail.com>
2019-09-19 00:05:54 +0800

    updating our app with a shop page, collection preview component, and collection item component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,8 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = () => (
-  <div>
-    <h1> HATS PAGE </h1>
-  </div>
-)
 
 class App extends Component {
 
@@ -17,7 +13,7 @@ class App extends Component {
       <div>
         <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route path='/hats' component={HatsPage} />
+          <Route path='/shop' component={ShopPage} />
         </Switch>
       </div>
     )
---
+++ src/pages/shop/shop.data.js
@@ -0,0 +1,230 @@
+import bp_hoodie_black from '../../resources/blackpink/hoodie.png';
+import ktl_hoodie_black from '../../resources/blackpink/kill_this_love_hoodie.png';
+import twice_yoy_hoodie from '../../resources/twice/twice_yoy_hoodie.png';
+import twice_signal_hoodie from '../../resources/twice/twice_signal_hoodie.jpg';
+import twice_kk_hoodie from '../../resources/twice/twice_kk_hoodie.jpg';
+const SHOP_DATA = [
+    {
+      id: 1,
+      title: 'Hoodies',
+      routeName: 'hoodies',
+      items: [
+        {
+          id: 1,
+          name: 'Blackpink Hoodie',
+          imageUrl: bp_hoodie_black,
+          price: 950
+        },
+        {
+          id: 2,
+          name: 'Kill This Love Hoodie',
+          imageUrl: ktl_hoodie_black,
+          price: 950
+        },
+        {
+          id: 3,
+          name: 'Yes or Yes Hoodie',
+          imageUrl: twice_yoy_hoodie,
+          price: 850
+        },
+        {
+          id: 4,
+          name: 'Signal Hoodie',
+          imageUrl: twice_signal_hoodie,
+          price: 850
+        },
+        {
+          id: 5,
+          name: 'Knock Knock Hoodie',
+          imageUrl: twice_kk_hoodie,
+          price: 850
+        }
+      ]
+    },
+    {
+      id: 2,
+      title: 'Sneakers',
+      routeName: 'sneakers',
+      items: [
+        {
+          id: 1,
+          name: 'Adidas NMD',
+          imageUrl: 'https://i.ibb.co/0s3pdnc/adidas-nmd.png',
+          price: 220
+        },
+        {
+          id: 2,
+          name: 'Adidas Yeezy',
+          imageUrl: 'https://i.ibb.co/dJbG1cT/yeezy.png',
+          price: 280
+        },
+        {
+          id: 3,
+          name: 'Black Converse',
+          imageUrl: 'https://i.ibb.co/bPmVXyP/black-converse.png',
+          price: 110
+        },
+        {
+          id: 4,
+          name: 'Nike White AirForce',
+          imageUrl: 'https://i.ibb.co/1RcFPk0/white-nike-high-tops.png',
+          price: 160
+        },
+        {
+          id: 5,
+          name: 'Nike Red High Tops',
+          imageUrl: 'https://i.ibb.co/QcvzydB/nikes-red.png',
+          price: 160
+        },
+        {
+          id: 6,
+          name: 'Nike Brown High Tops',
+          imageUrl: 'https://i.ibb.co/fMTV342/nike-brown.png',
+          price: 160
+        },
+        {
+          id: 7,
+          name: 'Air Jordan Limited',
+          imageUrl: 'https://i.ibb.co/w4k6Ws9/nike-funky.png',
+          price: 190
+        },
+        {
+          id: 8,
+          name: 'Timberlands',
+          imageUrl: 'https://i.ibb.co/Mhh6wBg/timberlands.png',
+          price: 200
+        }
+      ]
+    },
+    {
+      id: 3,
+      title: 'Jackets',
+      routeName: 'jackets',
+      items: [
+        {
+          id: 1,
+          name: 'Black Jean Shearling',
+          imageUrl: 'https://i.ibb.co/XzcwL5s/black-shearling.png',
+          price: 125
+        },
+        {
+          id: 2,
+          name: 'Blue Jean Jacket',
+          imageUrl: 'https://i.ibb.co/mJS6vz0/blue-jean-jacket.png',
+          price: 90
+        },
+        {
+          id: 3,
+          name: 'Grey Jean Jacket',
+          imageUrl: 'https://i.ibb.co/N71k1ML/grey-jean-jacket.png',
+          price: 90
+        },
+        {
+          id: 4,
+          name: 'Brown Shearling',
+          imageUrl: 'https://i.ibb.co/s96FpdP/brown-shearling.png',
+          price: 165
+        },
+        {
+          id: 5,
+          name: 'Tan Trench',
+          imageUrl: 'https://i.ibb.co/M6hHc3F/brown-trench.png',
+          price: 185
+        }
+      ]
+    },
+    {
+      id: 4,
+      title: 'Womens',
+      routeName: 'womens',
+      items: [
+        {
+          id: 1,
+          name: 'Blue Tanktop',
+          imageUrl: 'https://i.ibb.co/7CQVJNm/blue-tank.png',
+          price: 25
+        },
+        {
+          id: 2,
+          name: 'Floral Blouse',
+          imageUrl: 'https://i.ibb.co/4W2DGKm/floral-blouse.png',
+          price: 20
+        },
+        {
+          id: 3,
+          name: 'Floral Dress',
+          imageUrl: 'https://i.ibb.co/KV18Ysr/floral-skirt.png',
+          price: 80
+        },
+        {
+          id: 4,
+          name: 'Red Dots Dress',
+          imageUrl: 'https://i.ibb.co/N3BN1bh/red-polka-dot-dress.png',
+          price: 80
+        },
+        {
+          id: 5,
+          name: 'Striped Sweater',
+          imageUrl: 'https://i.ibb.co/KmSkMbH/striped-sweater.png',
+          price: 45
+        },
+        {
+          id: 6,
+          name: 'Yellow Track Suit',
+          imageUrl: 'https://i.ibb.co/v1cvwNf/yellow-track-suit.png',
+          price: 135
+        },
+        {
+          id: 7,
+          name: 'White Blouse',
+          imageUrl: 'https://i.ibb.co/qBcrsJg/white-vest.png',
+          price: 20
+        }
+      ]
+    },
+    {
+      id: 5,
+      title: 'Mens',
+      routeName: 'mens',
+      items: [
+        {
+          id: 1,
+          name: 'Camo Down Vest',
+          imageUrl: 'https://i.ibb.co/xJS0T3Y/camo-vest.png',
+          price: 325
+        },
+        {
+          id: 2,
+          name: 'Floral T-shirt',
+          imageUrl: 'https://i.ibb.co/qMQ75QZ/floral-shirt.png',
+          price: 20
+        },
+        {
+          id: 3,
+          name: 'Black & White Longsleeve',
+          imageUrl: 'https://i.ibb.co/55z32tw/long-sleeve.png',
+          price: 25
+        },
+        {
+          id: 4,
+          name: 'Pink T-shirt',
+          imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
+          price: 25
+        },
+        {
+          id: 5,
+          name: 'Jean Long Sleeve',
+          imageUrl: 'https://i.ibb.co/VpW4x5t/roll-up-jean-shirt.png',
+          price: 40
+        },
+        {
+          id: 6,
+          name: 'Burgundy T-shirt',
+          imageUrl: 'https://i.ibb.co/mh3VM1f/polka-dot-shirt.png',
+          price: 25
+        }
+      ]
+    }
+  ]
+
+  export default SHOP_DATA;
